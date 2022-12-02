dd1f2801cf18eb7a6d15f8fd2af715661ebfbc40
standardrhyme <asherkang.s@gmail.com>
2022-12-01 22:54:20 -0500

    Beginning a POST endpoint
    Co-authored-by: Jacob Petterle <johnluhot@users.noreply.github.com>

--- awscdk-minecraft_api/src/main.py
+++ awscdk-minecraft_api/src/main.py
@@ -6,3 +6,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+# pip install uvicorn
+
+# FastAPI: https://fastapi.tiangolo.com/tutorial/first-steps/
+# Rootski API docs: https://api.rootski.io/docs
+
+
+@app.post("/")
+async def
