6ed6ac1c7eb1972fffde15abe77c53ccdbe91a1e
Michael Pilosov <40366263+mathematicalmichael@users.noreply.github.com>
2022-12-03 01:58:06 -0700

    Update test_example.py

--- awscdk-minecraft/tests/test_example.py
+++ awscdk-minecraft/tests/test_example.py
@@ -2,13 +2,13 @@ import pytest
 from aws_cdk import App
 from aws_cdk.assertions import Template
 
-from cdk_metaflow.main import MyStack
+from cdk_minecraft import ExampleStack
 
 
 @pytest.fixture(scope="module")
 def template():
     app = App()
-    stack = MyStack(app, "my-stack-test")
+    stack = ExampleStack(app, "my-stack-test")
     template = Template.from_stack(stack)
     yield template
 
