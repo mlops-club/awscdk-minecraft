6dc004b57ab4944f0687a859c97622c6a795a6c7
phitoduck <eric.russia97+mlops-club@gmail.com>
2022-12-08 17:45:01 -0700

    fixes to Dockerfile and Job Definition

--- awscdk-minecraft/resources/awscdk-minecraft-server-deployer/app.py
+++ awscdk-minecraft/resources/awscdk-minecraft-server-deployer/app.py
@@ -1,11 +1,19 @@
 """Entrypoint for the AWS CDK Minecraft Server Deployer."""
+
 import os
 
 from aws_cdk import App, Environment
 from minecraft_server_deployer import ServerStack
 
-# for development, use account/region from cdk cli
-CDK_ENV = Environment(account=os.environ["AWS_ACCOUNT_ID"], region=os.getenv("AWS_REGION"))
+AWS_REGION = os.environ["AWS_REGION"]
+AWS_ACCOUNT_ID = os.environ["AWS_ACCOUNT_ID"]
+
+from datetime import datetime
+
+print(f"[{datetime.now()}] Running app.py for Account {AWS_ACCOUNT_ID}, Region {AWS_REGION}")
+
+# for development, use account/region from CDK CLI
+CDK_ENV = Environment(account=AWS_ACCOUNT_ID, region=AWS_REGION)
 APP = App()
 
 ServerStack(APP, "awscdk-minecraft-server", env=CDK_ENV)
--- awscdk-minecraft/src/cdk_minecraft/deploy_server_batch_job/job_definition.py
+++ awscdk-minecraft/src/cdk_minecraft/deploy_server_batch_job/job_definition.py
@@ -1,6 +1,7 @@
 """Job definition for the batch job that will deploy the Minecraft server on EC2."""
 from pathlib import Path
 
+from aws_cdk import Stack
 from aws_cdk import aws_batch_alpha as batch_alpha
 from aws_cdk import aws_ecr_assets as ecr_assets
 from aws_cdk import aws_ecs as ecs
@@ -9,12 +10,6 @@ from constructs import Construct
 
 THIS_DIR = Path(__file__).parent
 DOCKERIZED_AWS_CDK_BUILD_CONTEXT = (THIS_DIR / "../../../resources/awscdk-minecraft-server-deployer").resolve()
-
-# class ExampleStack(Construct):
-#     def __init__(
-#         self, scope: Construct, construct_id: str, **kwargs
-#     ) -> None:
-#         super().__init__(scope, construct_id, **kwargs)
 
 
 def make_minecraft_ec2_deployment__batch_job_definition(
@@ -38,6 +33,8 @@ def make_minecraft_ec2_deployment__batch_job_definition(
     execution_role: iam.Role = make_batch_execution_role(scope=scope, id_prefix=id_prefix)
     job_role: iam.Role = make_cdk_deployment_role(scope=scope, id_prefix=id_prefix)
 
+    stack = Stack.of(scope)
+
     return batch_alpha.JobDefinition(
         scope=scope,
         id=f"{id_prefix}CdkMinecraftEc2DeploymentJD",
@@ -46,12 +43,24 @@ def make_minecraft_ec2_deployment__batch_job_definition(
                 directory=str(DOCKERIZED_AWS_CDK_BUILD_CONTEXT),
                 platform=ecr_assets.Platform.LINUX_AMD64,
             ),
-            command=["cdk", "deploy", "--app", "'python3 /app/app.py'"],
+            command=["cdk", "deploy", "--app", "'python3 /app/app.py'", "--require-approval=never"],
             job_role=job_role,
             execution_role=execution_role,
             log_configuration=batch_alpha.LogConfiguration(
                 log_driver=batch_alpha.LogDriver.AWSLOGS,
+                options={
+                    # With the awslogs-group option, you can specify the log group that the awslogs log driver
+                    # sends its log streams to. If this isn't specified, aws/batch/job is used.
+                    "awslogs-group": "minecraft-server-deployment-job",
+                    # Specify whether you want the log group automatically created. If this option isn't specified, it defaults to false.
+                    "awslogs-create-group": "true",
+                },
             ),
+            assign_public_ip=True,
+            environment={
+                "AWS_ACCOUNT_ID": stack.account,
+                "AWS_REGION": stack.region,
+            },
         ),
         platform_capabilities=[batch_alpha.PlatformCapabilities.FARGATE],
     )
@@ -118,8 +127,70 @@ def make_batch_execution_role(scope: Construct, id_prefix: str) -> iam.Role:
                                 "ecr:BatchCheckLayerAvailability",
                                 "ecr:GetDownloadUrlForLayer",
                                 "ecr:BatchGetImage",
+                                # "logs:CreateLogStream",
+                                # "logs:PutLogEvents",
+                                # from AWS docs
+                                "ec2:DescribeAccountAttributes",
+                                "ec2:DescribeInstances",
+                                "ec2:DescribeInstanceAttribute",
+                                "ec2:DescribeSubnets",
+                                "ec2:DescribeSecurityGroups",
+                                "ec2:DescribeKeyPairs",
+                                "ec2:DescribeImages",
+                                "ec2:DescribeImageAttribute",
+                                "ec2:DescribeInstanceStatus",
+                                "ec2:DescribeSpotInstanceRequests",
+                                "ec2:DescribeSpotFleetInstances",
+                                "ec2:DescribeSpotFleetRequests",
+                                "ec2:DescribeSpotPriceHistory",
+                                "ec2:DescribeVpcClassicLink",
+                                "ec2:DescribeLaunchTemplateVersions",
+                                "ec2:CreateLaunchTemplate",
+                                "ec2:DeleteLaunchTemplate",
+                                "ec2:RequestSpotFleet",
+                                "ec2:CancelSpotFleetRequests",
+                                "ec2:ModifySpotFleetRequest",
+                                "ec2:TerminateInstances",
+                                "ec2:RunInstances",
+                                "autoscaling:DescribeAccountLimits",
+                                "autoscaling:DescribeAutoScalingGroups",
+                                "autoscaling:DescribeLaunchConfigurations",
+                                "autoscaling:DescribeAutoScalingInstances",
+                                "autoscaling:CreateLaunchConfiguration",
+                                "autoscaling:CreateAutoScalingGroup",
+                                "autoscaling:UpdateAutoScalingGroup",
+                                "autoscaling:SetDesiredCapacity",
+                                "autoscaling:DeleteLaunchConfiguration",
+                                "autoscaling:DeleteAutoScalingGroup",
+                                "autoscaling:CreateOrUpdateTags",
+                                "autoscaling:SuspendProcesses",
+                                "autoscaling:PutNotificationConfiguration",
+                                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                                "ecs:DescribeClusters",
+                                "ecs:DescribeContainerInstances",
+                                "ecs:DescribeTaskDefinition",
+                                "ecs:DescribeTasks",
+                                "ecs:ListAccountSettings",
+                                "ecs:ListClusters",
+                                "ecs:ListContainerInstances",
+                                "ecs:ListTaskDefinitionFamilies",
+                                "ecs:ListTaskDefinitions",
+                                "ecs:ListTasks",
+                                "ecs:CreateCluster",
+                                "ecs:DeleteCluster",
+                                "ecs:RegisterTaskDefinition",
+                                "ecs:DeregisterTaskDefinition",
+                                "ecs:RunTask",
+                                "ecs:StartTask",
+                                "ecs:StopTask",
+                                "ecs:UpdateContainerAgent",
+                                "ecs:DeregisterContainerInstance",
+                                "logs:CreateLogGroup",
                                 "logs:CreateLogStream",
                                 "logs:PutLogEvents",
+                                "logs:DescribeLogGroups",
+                                "iam:GetInstanceProfile",
+                                "iam:GetRole",
                             ],
                             "Resource": "*",
                         }
