5ae32624d8f318be144692074cf012b17eb24c77
Michael Pilosov <consistentbayes@gmail.com>
2022-12-08 21:27:50 -0700

    trying out diff things to connect to ec2

--- awscdk-minecraft/resources/awscdk-minecraft-server-deployer/src/minecraft_server_deployer/server_stack.py
+++ awscdk-minecraft/resources/awscdk-minecraft-server-deployer/src/minecraft_server_deployer/server_stack.py
@@ -3,6 +3,7 @@
 
 from aws_cdk import Stack
 from aws_cdk import aws_ec2 as ec2
+from aws_cdk import aws_iam as iam
 from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
@@ -30,12 +31,12 @@ class ServerStack(Stack):
 
         s3.Bucket(scope=self, id="MinecraftServer")
 
-        # set up an ec2 instance from an aws linux AMI and run a setup.sh script upon startup.
         _vpc = ec2.Vpc.from_lookup(scope=self, is_default=True, id="defaultVpc")
 
+        # set up security group to allow inbound traffic on port 25565 for anyone
         _sg = ec2.SecurityGroup(
             scope=self,
-            id="MinecraftServerInstanceSecurityGroup",
+            id="MinecraftServerSecurityGroup",
             vpc=_vpc,
             allow_all_outbound=True,
         )
@@ -49,6 +50,19 @@ class ServerStack(Stack):
             connection=ec2.Port.tcp(22),
             description="Allow inbound traffic on port 22",
         )
+        # allow all outbound traffic
+        _sg.add_egress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.all_traffic(),
+            description="Allow all outbound traffic",
+        )
+
+        # create iam role for ec2 instance
+        _iam_role = iam.Role(
+            scope=self,
+            id="MinecraftServerIamRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+        )
 
         ec2.Instance(
             scope=self,
@@ -58,10 +72,6 @@ class ServerStack(Stack):
             machine_image=ec2.MachineImage.latest_amazon_linux(),
             user_data=ec2.UserData.custom(
                 f"""#!/bin/bash
-                sudo yum update -y
-                sudo yum install -y docker docker-compose
-                sudo service docker start
-
                 cat EOF > /home/ec2-user/docker-compose.yml
                 version: '3.7'
                 services:
@@ -78,12 +88,30 @@ class ServerStack(Stack):
 
                 cat EOF > /home/ec2-user/setup.sh
                 #!/bin/bash
+                sudo yum update -y
+                sudo yum install -y docker docker-compose
+                sudo service docker start
+
+                # install ssm agent
+                sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+                sudo systemctl enable amazon-ssm-agent
+                sudo systemctl start amazon-ssm-agent
+                # install aws cli
+                sudo yum install -y python3
+                sudo pip3 install awscli --upgrade --user
+                # install aws s3 sync
+                sudo yum install -y s3fs-fuse
+
+                cd /home/ec2-user && \
                 sudo docker-compose up -d
+
                 EOF
 
                 sudo chmod +x /home/ec2-user/setup.sh
                 sudo /home/ec2-user/setup.sh
                 """
             ),
+            user_data_causes_replacement=True,
+            role=_iam_role,
             security_group=_sg,
         )
