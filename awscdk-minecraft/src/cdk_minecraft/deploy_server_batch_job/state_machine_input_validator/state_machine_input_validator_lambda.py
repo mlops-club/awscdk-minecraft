1323385d4e69bcfce6a47d4674e8d48454dceb6c
phitoduck <eric.riddoch@gmail.com>
2022-12-22 00:35:43 -0800

    added a lambda function to the state machine that validates the input event

--- awscdk-minecraft/src/cdk_minecraft/deploy_server_batch_job/state_machine_input_validator/state_machine_input_validator_lambda.py
+++ awscdk-minecraft/src/cdk_minecraft/deploy_server_batch_job/state_machine_input_validator/state_machine_input_validator_lambda.py
@@ -1,21 +1,35 @@
+from pathlib import Path
+
 import aws_cdk as cdk
 from aws_cdk import aws_lambda as lambda_
-from aws_cdk import aws_stepfunctions as sfn
+from aws_cdk import aws_lambda_python_alpha as lambda_python
 from constructs import Construct
 
-# construct
+THIS_DIR = Path(__file__).parent
+MC_SERVER_PROVISION_SFN__INPUT_VALIDATOR__SRC_DIR = THIS_DIR / "resources"
 
 
 def make_lambda_that_validates_input_of_the_provision_server_state_machine(
-    scope: Construct,
-) -> lambda_.Function:
+    scope: Construct, id_prefix: str
+) -> lambda_python.PythonFunction:
 
     # define the lambda function
-    return lambda_.Function(
+    return lambda_python.PythonFunction(
         scope,
-        "ProvisionMcServerInputValidator",
+        f"{id_prefix}ProvisionMcServerInputValidator",
+        entry=str(MC_SERVER_PROVISION_SFN__INPUT_VALIDATOR__SRC_DIR),
+        handler="handler",
+        index="index.py",
         runtime=lambda_.Runtime.PYTHON_3_8,
-        handler="index.handler",
-        code=lambda_.Code.from_asset("src/cdk_minecraft/deploy_server_batch_job"),
         timeout=cdk.Duration.seconds(30),
+        bundling=lambda_python.BundlingOptions(
+            image=cdk.DockerImage.from_registry(image="lambci/lambda:build-python3.8"),
+        )
+        # bundling=cdk.BundlingOptions(
+        #     # learn about this here:
+        #     # https://docs.aws.amazon.com/cdk/api/v1/python/aws_cdk.aws_lambda/README.html#bundling-asset-code
+        #     # Using this lambci image makes it so that dependencies with C-binaries compile correctly for the lambda runtime.
+        #     # The AWS CDK python images were not doing this. Relevant dependencies are: pandas, asyncpg, and psycogp2-binary.
+        #     image=cdk.DockerImage.from_registry(image="lambci/lambda:build-python3.8"),
+        # ),
     )
