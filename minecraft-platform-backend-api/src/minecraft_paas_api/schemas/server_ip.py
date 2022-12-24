58879c3cf07850e22b0f060179000086bc213bed
Jacob Petterle <Jacob.Petterle@bengroup.com>
2022-12-23 21:21:51 -0700

    add error logic (kept as is, may need to change when merging with Eric's)

--- minecraft-platform-backend-api/src/minecraft_paas_api/schemas/server_ip.py
+++ minecraft-platform-backend-api/src/minecraft_paas_api/schemas/server_ip.py
@@ -6,6 +6,6 @@ from pydantic import BaseModel, Field
 
 
 class ServerIpSchema(BaseModel):
-    """Response model for the `/minecraft_server_ip_address` endpoint."""
+    """Response model for the `/minecraft-server-ip-address` endpoint."""
 
     server_ip_address: ipaddress.IPv4Address = Field(description="IPv4 address of the minecraft server.")
