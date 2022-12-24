1192747b8c32ca8243b099086e0335a806e412c4
Jacob Petterle <Jacob.Petterle@bengroup.com>
2022-12-23 19:54:04 -0700

    Update settings attribute in aws_descriptor.py to match shortened settings names updated previously

--- minecraft-platform-backend-api/src/minecraft_paas_api/routes/aws_descriptor.py
+++ minecraft-platform-backend-api/src/minecraft_paas_api/routes/aws_descriptor.py
@@ -5,6 +5,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from minecraft_paas_api.aws.cloudformation import get_cloud_form_output_value
 from minecraft_paas_api.aws.step_functions import describe_state_machine, get_latest_statemachine_execution
+from minecraft_paas_api.schemas.server_ip import ServerIpSchema
 from minecraft_paas_api.settings import Settings
 from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
 
@@ -38,7 +39,7 @@ def get_statemachine(request: Request):
     """Get the latest execution of a state machine."""
     settings: Settings = load_settings_from_request_state(request)
     latest_execution: Optional[dict] = get_latest_statemachine_execution(
-        state_machine_arn=settings.provision_minecraft_server__state_machine__arn
+        state_machine_arn=settings.state_machine_arn
     )
 
     if latest_execution:
@@ -53,13 +54,13 @@ def get_statemachine(request: Request):
 def get_state_machine_status(request: Request):
     """Get the state machine status."""
     settings: Settings = load_settings_from_request_state(request)
-    return describe_state_machine(state_machine_arn=settings.provision_minecraft_server__state_machine__arn)
+    return describe_state_machine(state_machine_arn=settings.state_machine_arn)
 
 
-@ROUTER.get("/minecraft_server_ip_address")
+@ROUTER.get("/minecraft_server_ip_address", response_model=ServerIpSchema)
 def get_minecraft_server_ip_address(request: Request):
     """Get the minecraft server ip address."""
     settings: Settings = load_settings_from_request_state(request)
     return get_cloud_form_output_value(
-        settings.cloud_form_server_ip_output_key_name, settings.cloud_form_server_ip_output_key_name
+        settings.cloud_formation_stack_name, settings.cloud_formation_server_ip_output_key_name
     )
