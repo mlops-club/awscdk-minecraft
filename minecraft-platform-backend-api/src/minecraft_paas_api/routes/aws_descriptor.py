f15f59d65c7177973e12bd6a1a40c4e06cdb5df5
Jacob Petterle <Jacob.Petterle@bengroup.com>
2022-12-23 19:06:56 -0700

    add get endpoint for retrieving ec2 ip addressand funciton to retrive the ip from the cloud formation output

--- minecraft-platform-backend-api/src/minecraft_paas_api/routes/aws_descriptor.py
+++ minecraft-platform-backend-api/src/minecraft_paas_api/routes/aws_descriptor.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Optional
 
 from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
+from minecraft_paas_api.aws.cloudformation import get_cloud_form_output_value
 from minecraft_paas_api.aws.step_functions import describe_state_machine, get_latest_statemachine_execution
 from minecraft_paas_api.settings import Settings
 from starlette.status import HTTP_200_OK, HTTP_404_NOT_FOUND
@@ -20,11 +21,22 @@ def replace_datetimes_in_dict_with_strings(data: Dict[str, Any]) -> Dict[str, An
     return data
 
 
+def load_settings_from_request_state(request: Request) -> Settings:
+    """
+    Load settings from request state.
+
+    :param request: request object to load the settings from
+
+    :return: settings object from the request state object
+    """
+    app_state = request.app.state
+    return app_state.settings
+
+
 @ROUTER.get("/latest-execution")
 def get_statemachine(request: Request):
     """Get the latest execution of a state machine."""
-    app_state = request.app.state
-    settings: Settings = app_state.settings
+    settings: Settings = load_settings_from_request_state(request)
     latest_execution: Optional[dict] = get_latest_statemachine_execution(
         state_machine_arn=settings.provision_minecraft_server__state_machine__arn
     )
@@ -39,7 +51,15 @@ def get_statemachine(request: Request):
 
 @ROUTER.get("/state-machine-status")
 def get_state_machine_status(request: Request):
-    """Get the stat machine status."""
-    app_state = request.app.state
-    settings: Settings = app_state.settings
+    """Get the state machine status."""
+    settings: Settings = load_settings_from_request_state(request)
     return describe_state_machine(state_machine_arn=settings.provision_minecraft_server__state_machine__arn)
+
+
+@ROUTER.get("/minecraft_server_ip_address")
+def get_minecraft_server_ip_address(request: Request):
+    """Get the minecraft server ip address."""
+    settings: Settings = load_settings_from_request_state(request)
+    return get_cloud_form_output_value(
+        settings.cloud_form_server_ip_output_key_name, settings.cloud_form_server_ip_output_key_name
+    )
