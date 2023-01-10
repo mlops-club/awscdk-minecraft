afed70d542f181b5db82bec07ff155a39562b3f1
phitoduck <eric.riddoch@gmail.com>
2023-01-10 00:47:25 -0700

    removed the POC frontend; it's retriavable in the git history

--- minecraft-platform-frontend-poc/src/UI/RootDiv.js
+++
@@ -1,9 +0,0 @@
-import './RootDiv.css';
-
-function RootDiv(props){
-    const classes = 'ServerOfflineHeader ' + props.className;
-    return <div className={classes}>{props.children}</div>;
-
-}
-
-export default RootDiv;
--- minecraft-platform-frontend-poc/src/components/ServerOffline/BucketStatus.js
+++
@@ -1,9 +0,0 @@
-import './BucketStatus.css';
-
-function BucketStatus(){
-    return (
-        <div>Bucket Status</div>
-    )
-}
-
-export default BucketStatus;
